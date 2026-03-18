7fd39b0362304d72a9b6dfd812c2b387dbe84e68
SysPoe <panddplayz@outlook.com>
2026-03-18 19:06:53 +1000

    Very minor updatezzzz

--- mover.js
+++ mover.js
@@ -164,7 +164,7 @@ export class MyMover {
     set(vals) {
         // process.stdout.write(`\r\x1b[0;0H\x1b[2J${Object.entries(vals).map(([k, v]) => `${k}: ${v}`).join("\n")}`);
         vals = Object.fromEntries(Object.entries(vals).map(([k, v]) => [this.CHANNELS[k], v]));
-        getDmx().setChannels(vals);
+        this.setChannels(vals);
     }
 
     /**
@@ -172,6 +172,7 @@ export class MyMover {
      * @param {Record<number, number>} channels An object mapping channel IDs to their values.
      */
     setChannels(channels) {
+        // console.log(channels);
         getDmx().setChannels(channels);
     }
 
