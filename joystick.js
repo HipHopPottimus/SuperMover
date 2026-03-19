9bc85c622a45ae02df13d6e6936a55341506c3a4
Owen Agnew <owenagnew11@outlook.com>
2026-03-19 18:54:55 +1000

    Move joystick functionality into its own file and clean up main.js

---
+++ joystick.js
@@ -0,0 +1,92 @@
+import usb from "usb";
+
+const DEADZONE = 10;
+const SENSITIVITY = 1;
+const NON_LINEAR_EXPONENT = 3;
+const INVERT_X = false;
+const INVERT_Y = false;
+
+class Joystick {
+    /** @type {usb.usb.Device}*/
+    device;
+
+    /** @type {usb.Endpoint} */
+    endpoint;
+    
+    /** @type {number[]} */
+    rawData;
+
+    /**@type {Function} */
+    onData;
+
+    //more accessible processed joystick data
+    
+    /**@type {number}*/
+    x;
+    /**@type {number}*/
+    y;
+
+    /**@type {number}*/
+    dX;
+    /**@type {number}*/
+    dY;
+
+    /**@type {number}*/
+    throttle;
+
+    constructor(vendorId, productId) {
+        this.device = usb.findByIds(vendorId, productId);
+        if (!this.device) throw new Error("Joystick not found!");
+    
+        this.device.open();
+    
+        if (!this.device.interfaces?.[0]) throw new Error("No interfaces on joystick!");
+        let iface = this.device.interfaces[0];
+    
+        try { if (iface.isKernelDriverActive()) iface.detachKernelDriver(); } catch { }
+        iface.claim();
+    
+        this.endpoint = iface.endpoints.find(e => e.direction === "in");
+        if (!this.endpoint) throw new Error("No IN endpoint found");
+    
+        this.endpoint.on("data", (data) => {
+            this.rawData = data;
+            this.throttle = 255 - data[2];
+
+            this.dX = nonLinearMapping(applyDeadzone((INVERT_X * 2 - 1) * (data[0] - 127))) * SENSITIVITY;
+            this.dY = nonLinearMapping(applyDeadzone((INVERT_Y * 2 - 1) * (data[1] - 127))) * SENSITIVITY;
+
+            if(this.onData) this.onData();
+        
+            // console.log(`Got: ${data[0]}, ${data[1]} | dX: ${dX}, dY: ${dY}`);
+        });
+
+        setInterval(() => {
+            this.x += this.dX * UPDATE_INTERVAL / 1000;
+            this.y += this.dY * UPDATE_INTERVAL / 1000;
+
+            this.x = clamp(this.x, 0, 255);
+            this.y = clamp(this.y, 0, 255);
+        }, UPDATE_INTERVAL);
+
+        endpoint.startPoll();
+    }
+}
+
+function applyDeadzone(value) {
+    if (Math.abs(value) < DEADZONE) return 0;
+    return value;
+}
+
+function nonLinearMapping(value) {
+    value = value / 127; // Normalize to -1 to 1
+    return Math.sign(value) * Math.pow(Math.abs(value), NON_LINEAR_EXPONENT) * 127; // Non-linear mapping and scale back to -127 to 127
+}
+
+function clamp(v, lo, hi) {
+    return Math.max(lo, Math.min(hi, v));
+}
+
+export default {
+    Joystick
+}
--- main.js
+++ main.js
@@ -1,82 +1,21 @@
-import usb from "usb";
-import Mover from "./mover.js";
+import mover from "./mover.js";
+import joystick from "./joystick.js";
 
-const mover1 = new Mover.Mover(1, process.env.debug === "true");
 
-const DEADZONE = 10;
-const SENSITIVITY = 1;
-const UPDATE_INTERVAL = 30; // ms
-const NON_LINEAR_EXPONENT = 3;
-const INVERT_X = false;
-const INVERT_Y = false;
+const mover1 = new mover.Mover(1, process.env.debug === "true");
 
-function getJoystick(vendorId, productId) {
-    let device = usb.findByIds(vendorId, productId);
-    if (!device) throw new Error("Joystick not found!");
+const joystick1 = new joystick.Joystick(0x046d, 0xc214);
 
-    device.open();
-
-    if (!device.interfaces?.[0]) throw new Error("No interfaces on joystick!");
-    let iface = device.interfaces[0];
-
-    try { if (iface.isKernelDriverActive()) iface.detachKernelDriver(); } catch { }
-    iface.claim();
-
-    let endpoint = iface.endpoints.find(e => e.direction === "in");
-    if (!endpoint) throw new Error("No IN endpoint found");
-
-    endpoint.startPoll();
-
-    return endpoint;
-}
-
-let joystick = getJoystick(0x046d, 0xc214);
-
-let dX = 0, dY = 0, x = 0, y = 0;
-
-function applyDeadzone(value) {
-    if (Math.abs(value) < DEADZONE) return 0;
-    return value;
-}
-
-function nonLinearMapping(value) {
-    value = value / 127; // Normalize to -1 to 1
-    return Math.sign(value) * Math.pow(Math.abs(value), NON_LINEAR_EXPONENT) * 127; // Non-linear mapping and scale back to -127 to 127
-}
-
-joystick.on("data", (data) => {
-    const throttle = 255 - data[2];
-
+joystick1.onData = () => {
     mover1.set({
-        Dimmer: throttle,
+        Pan: joystick1.x,
+        Tilt: joystick1.y,
     });
-
-    if (data[3]) 
-        mover1.set({ ColorWheel: Math.floor(Math.log2(data[3])) * 8 });
-
-    dX = nonLinearMapping(applyDeadzone((INVERT_X * 2 - 1) * (data[0] - 127))) * SENSITIVITY;
-    dY = nonLinearMapping(applyDeadzone((INVERT_Y * 2 - 1) * (data[1] - 127))) * SENSITIVITY;
-
-    // console.log(`Got: ${data[0]}, ${data[1]} | dX: ${dX}, dY: ${dY}`);
-});
-
-setInterval(() => {
-    x += dX * UPDATE_INTERVAL / 1000;
-    y += dY * UPDATE_INTERVAL / 1000;
-    x = clamp(x, 0, 255);
-    y = clamp(y, 0, 255);
-    // console.log(x, y);
     mover1.set({
-        Pan: x,
-        Tilt: y,
+        Dimmer: joystick1.throttle,
     });
-}, UPDATE_INTERVAL);
-
-function clamp(v, lo, hi) {
-    return Math.max(lo, Math.min(hi, v));
+    if (data[3]) mover1.set({ ColorWheel: Math.floor(Math.log2(data[3])) * 8 });
 }
-
-console.log("Sending");
 
 import repl from "repl";
 const rl = repl.start({
@@ -87,5 +26,5 @@ const rl = repl.start({
 
 Object.assign(rl.context, {
     mover1,
-    Mover
-})
+    mover
+});
